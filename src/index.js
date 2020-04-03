9d5a4ed382ff7ec9175364563cf9defec26be309
FOBABS <fobabs2win@rocketmail.com>
2020-04-03 22:55:31 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,15 +5,28 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" 
+      onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
       </button>
     );
   }
 }
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square value={this.state.squares[i]} 
+      onClick={() => this.handleClick(i)} 
+      />
+    );
   }
 
   render() {
@@ -56,7 +69,6 @@ class Game extends React.Component {
     )
   }
 }
-
 
 ReactDOM.render(
   <Game />,
