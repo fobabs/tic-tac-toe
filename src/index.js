caa55f401cfad0af526ff3c4c4ca40bc05602e5a
FOBABS <fobabs2win@rocketmail.com>
2020-04-07 17:48:22 +0100

    Create handleClick function

--- src/index.js
+++ src/index.js
@@ -21,6 +21,12 @@ class Board extends React.Component {
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
     return (
       <Square value={this.state.squares[i]} 
